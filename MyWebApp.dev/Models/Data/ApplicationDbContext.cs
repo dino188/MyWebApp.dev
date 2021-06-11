378e3c77de853b2e965d4c3c4032971e93412938
dinoc <dinoc@DESKTOP-4HCGL5S>
2021-06-11 23:43:55 +0200

    Add project files.

---
+++ MyWebApp.dev/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.dev.Models.Data.Models;
+
+namespace MyWebApp.dev.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+    }
+}
